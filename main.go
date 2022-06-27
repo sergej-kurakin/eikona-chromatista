36dcbca310b026c5c475bfd4a6fe26d600991a48
Sergej Kurakin <sergej@kurakin.info>
2022-06-27 21:07:15 +0300

    Add gray chrome, negative, gray chrome negative, grayscale

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func check(err error) {
 
 func main() {
 
-	var processors [13]Processor
+	var processors [17]Processor
 	processors[0] = Processor{suffix: "gray", color_processor: make_gray_color}
 	processors[1] = Processor{suffix: "rbg", color_processor: make_RBG_color}
 	processors[2] = Processor{suffix: "gbr", color_processor: make_GBR_color}
@@ -40,6 +40,10 @@ func main() {
 	processors[10] = Processor{suffix: "g", color_processor: make_G_color}
 	processors[11] = Processor{suffix: "b", color_processor: make_B_color}
 	processors[12] = Processor{suffix: "rnd", color_processor: make_random_color}
+	processors[13] = Processor{suffix: "photometric_grayscale", color_processor: make_photometric_grayscale}
+	processors[14] = Processor{suffix: "photometric_graychrome", color_processor: make_photometric_graychrome}
+	processors[15] = Processor{suffix: "photometric_graychrome_negative", color_processor: make_photometric_graychrome_negative}
+	processors[16] = Processor{suffix: "negative", color_processor: make_negative}
 
 	if len(os.Args) < 2 {
 		log.Fatalln("Image path is required")
@@ -213,6 +217,64 @@ func make_random_color(pixel color.Color) color.Color {
 	processor_num := rand.Intn(len(processors))
 
 	return processors[processor_num](pixel)
+}
+
+func make_photometric_grayscale(pixel color.Color) color.Color {
+	originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+	// Y = 0.2126 R + 0.7152 G + 0.0722 B
+	Y := 0.2126*float32(originalColor.R) + 0.7152*float32(originalColor.G) + 0.0722*float32(originalColor.B)
+	c := color.RGBA{
+		R: uint8(Y), G: uint8(Y), B: uint8(Y), A: originalColor.A,
+	}
+
+	return c
+}
+
+func make_photometric_graychrome(pixel color.Color) color.Color {
+	originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+	// Y = 0.2126 R + 0.7152 G + 0.0722 B
+	Y := 0.2126*float32(originalColor.R) + 0.7152*float32(originalColor.G) + 0.0722*float32(originalColor.B)
+
+	k := uint8(Y)
+	var c color.RGBA
+
+	// 0 < x <= 64 -- 0
+	// 64 < x <= 128 -- 85
+	// 128 < x <= 192 -- 180
+	// 192 < x <= 255 -- 255
+
+	if k <= 64 {
+		c = color.RGBA{
+			R: 0, G: 0, B: 0, A: originalColor.A,
+		}
+	} else if k <= 128 {
+		c = color.RGBA{
+			R: 85, G: 85, B: 85, A: originalColor.A,
+		}
+	} else if k <= 192 {
+		c = color.RGBA{
+			R: 180, G: 180, B: 180, A: originalColor.A,
+		}
+	} else {
+		c = color.RGBA{
+			R: 255, G: 255, B: 255, A: originalColor.A,
+		}
+	}
+
+	return c
+}
+
+func make_negative(pixel color.Color) color.Color {
+	originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+	c := color.RGBA{
+		R: 255 - originalColor.R, G: 255 - originalColor.G, B: 255 - originalColor.B, A: originalColor.A,
+	}
+	return c
+}
+
+func make_photometric_graychrome_negative(pixel color.Color) color.Color {
+	graychrome_pixel := make_photometric_graychrome(pixel)
+	return make_negative(graychrome_pixel)
 }
 
 func newFileName(imgPath string, suffix string) string {
